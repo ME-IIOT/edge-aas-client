e5d5108912cfd34d4cdbfe38e5e53f858a90ba24
Thuy Dang <xuan.thuy.dang@yacoub.de>
2023-10-04 13:06:50 +0200

    Update interface models

--- aas_edge_client/api/interfaces/models.py
+++ aas_edge_client/api/interfaces/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 class Interface(models.Model):
     IPv4Address = models.GenericIPAddressField(protocol='IPv4')
-    NetworkMask = models.GenericIPAddressField(protocol='IPv4', null=True, blank=True)
     IPv4SubnetMask = models.GenericIPAddressField(protocol='IPv4', null=True, blank=True)
     Name = models.CharField(max_length=255,null=True, blank=True)
     LinkSpeedReceiveTransmit = models.CharField(max_length=255, null=True, blank=True)
-    LinkLocalIPv6Address = models.GenericIPAddressField(protocol='IPv6', null=True, blank=True)
     IPv4DNSServers = models.CharField(max_length=255, null=True, blank=True)
     PrimaryDNSSuffix = models.CharField(max_length=255, null=True, blank=True)
     interface_id = models.CharField(max_length=255, null=True, blank=True)
