a2d285901da0583884ec1cb9ada4450a890feab1
manhlinhphan <manhlinhphan@debian.myguest.virtualbox.org>
2023-10-05 12:35:01 +0200

    parser function

--- aas_edge_client/submodels_template/parser_submodel.py
+++ aas_edge_client/submodels_template/parser_submodel.py
@@ -36,3 +36,49 @@ def transform_response(inputResponse, templateFilePath):
         return None
     
     return templateJson
+
+"value of elem key is in submodel/<submodel_id>/elements/<elem_id>/deep is a dictionary, but put it in a list (only 1 element) to match the recursive structure"
+def django_response_2_aas_SM_element(djangoJSON, templateJSON):
+    '''Required inputResponse and templateJSON need to be same structure of submodelElements and Properties'''
+    
+#    for key, value in inputResponseJSON.items():
+    for index, (key, value) in enumerate(djangoJSON.items()):
+        if isinstance(value, dict):
+            django_response_2_aas_SM_element(value, templateJSON[index]["value"])
+        else:
+            templateJSON[index]["value"] = value
+
+    return templateJSON
+        
+
+def aas_SM_element_2_django_response(templateJSON):
+    #Recursive
+    # for element in inputResponseJSON: # for the SubmodelElements, there is only one element, but need to do this for recursive function
+    #     for elementKey, elementValue  in element.items():
+    djangoJSON = {} # init
+    for element in templateJSON:
+        if isinstance(element["value"], list):
+            djangoJSON[element["idShort"]] = aas_SM_element_2_django_response(element["value"])
+        else:
+            djangoJSON[element["idShort"]] = element["value"]
+
+    return djangoJSON
+
+inputResponseJSON = []
+with open("inputJSON.json", 'r') as file:
+    inputResponseJSON = json.load(file)
+
+
+outputResponseJSON = []
+with open("NetworkSetting.json", 'r') as file:
+    outputResponseJSON = json.load(file)
+
+# print(inputResponseJSON)
+# print(outputResponseJSON)
+
+# print(json.dumps(django_response_2_aas_SM_element(inputResponseJSON, outputResponseJSON)))
+print(json.dumps(aas_SM_element_2_django_response(outputResponseJSON)))
+
+
+    
+    
